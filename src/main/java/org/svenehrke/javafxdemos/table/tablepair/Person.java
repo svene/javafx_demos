3b1982bef0c74ceb2c89d0ab4c9ca95f9c6722ea
Sven Ehrke <sven.ehrke@canoo.com>
2013-12-22 11:26:36 +0100

    row sync: started with webview

--- src/main/java/org/svenehrke/javafxdemos/table/tablepair/Person.java
+++ src/main/java/org/svenehrke/javafxdemos/table/tablepair/Person.java
@@ -11,20 +11,21 @@ public class Person {
 	private final BetterStringProperty name3 = new BetterStringProperty();
 	private final BetterStringProperty name4 = new BetterStringProperty();
 
+
 	public static final String LONG_TEXT = "long_text";
+//	public static final String ROW_HEIGHT = "row_height";
 	private final RowItemInfo rowItemInfo = new RowItemInfo();
 
 
 	public Person(final int rowIdx) {
 
-		rowItemInfo.addProperty(LONG_TEXT, a -> isLongText(a.getValue()), (b1, b2) -> b1 || b2);
-		rowItemInfo.bind(Arrays.asList(name1, name2, name3, name4));
+//		rowItemInfo.addProperty(ROW_HEIGHT, a -> isLongText(a.getValue()), (Double d1, Double d2) -> Double.valueOf(d1.intValue() > d2.intValue() ? d1.intValue() : d2.intValue()));
+		rowItemInfo.addProperty(LONG_TEXT, a -> isLongText(a.getValue()), (b1, b2) -> b1 || b2);		rowItemInfo.bind(Arrays.asList(name1, name2, name3, name4));
 
 		name1.setValue(valueFrom(1, rowIdx));
 		name2.setValue(valueFrom(2, rowIdx));
 		name3.setValue(valueFrom(3, rowIdx));
 		name4.setValue(valueFrom(4, rowIdx));
-
 	}
 
 	private static boolean isLongText(final String item) {
@@ -52,30 +53,21 @@ public class Person {
 	}
 
 	private String valueFrom(final int colIdx, final int rowIdx) {
-		String s = "";
-		s = String.format("%d %030d", rowIdx, 0);
-		if (colIdx == 1) {
-			if (rowIdx % 3 == 0) {
-				return s;
-			}
+		if (colIdx == 3) {
+			int textIdx = rowIdx % 3;
+			return LONG_TEXT_ARRAY[textIdx];
 		}
-		else if (colIdx == 3) {
-			if (rowIdx % 5 == 0) {
-				return s;
-			}
-		}
-		else if (colIdx == 2) {
-			if (rowIdx % 4 == 0) {
-				return s;
-			}
-		}
-		else if (colIdx == 4) {
-			if (rowIdx % 7 == 0) {
-				return s;
-			}
-		}
-		return "name " + colIdx + " / " + rowIdx;
+		return LONG_TEXT_ARRAY[0];
 	}
 
+	private static String[] LONG_TEXT_ARRAY = new String[]{
+		wrappedHtml("{0} Dies ist ein nicht so langer Text"),
+		"{0} Dies ist ein mittellanger Text, der schon ein ziemlich blinder Blindtext ist, und nichts bedeutet. Damit er auch sicher einmal umgebrochen wird, müssen wir noch etwas labern",
+		"{0} Dies ist ein ganz, ganz, ganz langer Text, der schon ein ziemlich blinder Blindtext ist. Dieser Text hat keine inhaltliche Aussage. Dieser ist noch vieeeel länger, als der andere und wird deswegen sicher umgebrochen. Da das Fenster sehr breit ist, müssen wir noch einen viel längeren Text schreiben. Eigentlich wollte ich ja, dass er zweimal umgebrochen wird, schaun wir mal ob es reicht"
+	};
+
+	private static String wrappedHtml(final String htmlSnippet) {
+		return "<html><body style='background-color: red'><div id='bodyDivId'>" + htmlSnippet + "</div></body></html>";
+	}
 
 }
--- src/main/java/org/svenehrke/javafxdemos/table/tablepair/RowSizeInfo.java
+++ src/main/java/org/svenehrke/javafxdemos/table/tablepair/RowSizeInfo.java
@@ -1,6 +1,8 @@
 package org.svenehrke.javafxdemos.table.tablepair;
 
-import javafx.beans.property.*;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 
 import java.util.HashMap;
@@ -9,19 +11,19 @@ import java.util.Map;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
-public class RowItemInfo {
+public class RowSizeInfo {
 
-	private final Map<String, BooleanProperty> properties = new HashMap<>();
-	private final Map<String, Function<StringProperty, Boolean>> mappers = new HashMap<>();
-	private final Map<String, BinaryOperator<Boolean>> reducers = new HashMap<>();
+/*
+	private final DoubleProperty rowSize = new SimpleDoubleProperty();
+	private final Function<StringProperty, Double> mapper;
+	private final BinaryOperator<Double> reducer;
 
-	public void addProperty(String key, Function<StringProperty, Boolean> mapper, BinaryOperator<Boolean> reducer) {
-		properties.put(key, new SimpleBooleanProperty());
-		this.mappers.put(key, mapper);
+	public void addProperty(String key, Function<StringProperty, Double> mapper, BinaryOperator<Double> reducer) {
+		this.mapper = mapper;
 		this.reducers.put(key, reducer);
 	}
 
-	public Map<String, BooleanProperty> getProperties() {
+	public Map<String, DoubleProperty> getProperties() {
 		return properties;
 	}
 
@@ -29,7 +31,7 @@ public class RowItemInfo {
 
 		// Listen on each attribute for change. In case of a attribute change change row property:
 		attributes.forEach(a -> a.addListener((ChangeListener<String>) (s, o, n) -> {
-			properties.keySet().forEach(key -> properties.get(key).setValue(attributes.stream().map(mappers.get(key)).reduce(reducers.get(key)).get()));
+			rowSize.setValue(attributes.stream().map(mappers.get(key)).reduce(reducers.get(key)).get()));
 		}));
 
 		// In case of a row property change fire ValueChangeEvent on all attributes:
@@ -39,5 +41,6 @@ public class RowItemInfo {
 		}));
 
 	}
+*/
 
 }
