4e448c4cae49a8044c7e98d0ba295074b1c7a3a9
Sven Ehrke <sven.ehrke@canoo.com>
2014-11-01 10:07:45 +0100

    part5: styles

--- javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/address/Main.java
+++ javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/address/Main.java
@@ -3,14 +3,22 @@ package org.svenehrke.javafxdemos.address;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.controlsfx.dialog.Dialogs;
 import org.svenehrke.javafxdemos.address.model.Person;
+import org.svenehrke.javafxdemos.address.model.PersonListWrapper;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.prefs.Preferences;
 
 /**
  * Like 'Main' but the pane is loaded explicitly which allows to configure the controller (GreetController2 in this example) in the code
@@ -43,6 +51,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("AddressApp");
+
+		this.primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("/Address_Book.png")));
 
 		initRootLayout();
 		showPersonOverview();
@@ -79,5 +89,101 @@ public class Main extends Application {
 		rootLayout.setCenter(personOverview);
 
 	}
+
+	/**
+	 * Returns the person file preference, i.e. the file that was last opened.
+	 * The preference is read from the OS specific registry. If no such
+	 * preference can be found, null is returned.
+	 *
+	 * @return
+	 */
+	public File getPersonFilePath() {
+		Preferences prefs = Preferences.userNodeForPackage(Main.class);
+		String filePath = prefs.get("filePath", null);
+		if (filePath != null) {
+			return new File(filePath);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Sets the file path of the currently loaded file. The path is persisted in
+	 * the OS specific registry.
+	 *
+	 * @param file the file or null to remove the path
+	 */
+	public void setPersonFilePath(File file) {
+		Preferences prefs = Preferences.userNodeForPackage(Main.class);
+		if (file != null) {
+			prefs.put("filePath", file.getPath());
+
+			// Update the stage title.
+			primaryStage.setTitle("AddressApp - " + file.getName());
+		} else {
+			prefs.remove("filePath");
+
+			// Update the stage title.
+			primaryStage.setTitle("AddressApp");
+		}
+	}
+
+	/**
+	 * Loads person data from the specified file. The current person data will
+	 * be replaced.
+	 *
+	 * @param file
+	 */
+	public void loadPersonDataFromFile(File file) {
+		try {
+			JAXBContext context = JAXBContext
+				.newInstance(PersonListWrapper.class);
+			Unmarshaller um = context.createUnmarshaller();
+
+			// Reading XML from the file and unmarshalling.
+			PersonListWrapper wrapper = (PersonListWrapper) um.unmarshal(file);
+
+			model.getPersonData().clear();
+			model.getPersonData().addAll(wrapper.getPersons());
+
+			// Save the file path to the registry.
+			setPersonFilePath(file);
+
+		} catch (Exception e) { // catches ANY exception
+			Dialogs.create()
+				.title("Error")
+				.masthead("Could not load data from file:\n" + file.getPath())
+				.showException(e);
+		}
+	}
+
+	/**
+	 * Saves the current person data to the specified file.
+	 *
+	 * @param file
+	 */
+	public void savePersonDataToFile(File file) {
+		try {
+			JAXBContext context = JAXBContext
+				.newInstance(PersonListWrapper.class);
+			Marshaller m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			// Wrapping our person data.
+			PersonListWrapper wrapper = new PersonListWrapper();
+			wrapper.setPersons(model.getPersonData());
+
+			// Marshalling and saving XML to the file.
+			m.marshal(wrapper, file);
+
+			// Save the file path to the registry.
+			setPersonFilePath(file);
+		} catch (Exception e) { // catches ANY exception
+			Dialogs.create().title("Error")
+				.masthead("Could not save data to file:\n" + file.getPath())
+				.showException(e);
+		}
+	}
+
 
 }
