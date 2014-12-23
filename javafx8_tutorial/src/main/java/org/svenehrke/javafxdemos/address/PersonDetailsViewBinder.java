ef960220b387587905a5b06e0449a316da3310bd
Sven Ehrke <sven.ehrke@canoo.com>
2014-12-23 10:53:18 +0100

    moved logic of new/edit/delete commands into PersonDetailsViewBinder and deleted the command classes

--- javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/address/PersonDetailsViewBinder.java
+++ javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/address/PersonDetailsViewBinder.java
@@ -1,9 +1,8 @@
 package org.svenehrke.javafxdemos.address;
 
 import javafx.beans.binding.Bindings;
-import org.svenehrke.javafxdemos.address.commandhandler.DeletePersonCommandHandler;
-import org.svenehrke.javafxdemos.address.commandhandler.EditPersonCommandHandler;
-import org.svenehrke.javafxdemos.address.commandhandler.NewPersonCommandHandler;
+import javafx.stage.Stage;
+import org.controlsfx.dialog.Dialogs;
 import org.svenehrke.javafxdemos.address.model.Person;
 import org.svenehrke.javafxdemos.infra.Mate;
 
@@ -14,9 +13,9 @@ public class PersonDetailsViewBinder {
 		view.firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
 		view.lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
 
-		view.newButton.setOnAction(event -> new NewPersonCommandHandler(mate.getPrimaryStage(), mate.getModel()).run());
-		view.editButton.setOnAction(event -> new EditPersonCommandHandler(mate.getPrimaryStage(), mate.getModel()).run());
-		view.deleteButton.setOnAction(event -> new DeletePersonCommandHandler(mate.getModel()).run());
+		view.newButton.setOnAction(event -> handleNewPerson(mate.getPrimaryStage(), mate.getModel()));
+		view.editButton.setOnAction(event -> handleEditPerson(mate.getPrimaryStage(), mate.getModel()) );
+		view.deleteButton.setOnAction(event -> handleDelete(mate.getModel()) );
 
 		Person cp = mate.getModel().currentPerson;
 		view.firstNameLabel.textProperty().bind(cp.firstNameProperty());
@@ -33,6 +32,49 @@ public class PersonDetailsViewBinder {
 
 		view.personTable.setItems(mate.getModel().getPersonData());
 		view.personTable.getSelectionModel().selectedIndexProperty().addListener((s, o, n) -> mate.getModel().selectedModelIndex.setValue(n));
+
+	}
+
+
+	private static void handleNewPerson(Stage primaryStage1, Model model1) {
+		model1.workPerson.populateFromPerson(model1.emptyPerson);
+		model1.editModeProperty.setValue(Model.EditMode.NEW);
+		PersonDialogs.showPersonDialog(model1, primaryStage1);
+	}
+
+	/**
+	 * Opens a dialog to edit details for the specified person. If the user
+	 * clicks OK, the changes are saved into the provided person object and true
+	 * is returned.
+	 */
+	private static void handleEditPerson(Stage primaryStage1, Model model1) {
+		if (model1.currentPerson != null) {
+			model1.getWorkPerson().populateFromPerson(model1.currentPerson);
+			model1.editModeProperty.setValue(Model.EditMode.EDIT);
+			PersonDialogs.showPersonDialog(model1, primaryStage1);
+		} else {
+			// Nothing selected.
+			System.out.println("nothing selected");
+			Dialogs.create()
+				.title("No Selection")
+				.masthead("No Person Selected")
+				.message("Please select a person in the table.")
+				.showWarning();
+		}
+	}
+
+	private static void handleDelete(Model model1) {
+		int selectedIndex = model1.selectedModelIndex.intValue();
+		if (selectedIndex >= 0) {
+			model1.getPersonData().remove(selectedIndex);
+		} else {
+			// Nothing selected.
+			Dialogs.create()
+				.title("No Selection")
+				.masthead("No Person Selected")
+				.message("Please select a person in the table.")
+				.showWarning();
+		}
 
 	}
 }
