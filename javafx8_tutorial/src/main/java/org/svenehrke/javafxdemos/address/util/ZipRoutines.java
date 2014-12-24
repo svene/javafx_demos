f0801fcf05cac426e32cf9bf917ef75b50466c53
Sven Ehrke <sven.ehrke@canoo.com>
2014-12-24 14:13:31 +0100

    zip routines

--- javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/address/util/ZipRoutines.java
+++ javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/address/util/ZipRoutines.java
@@ -1,4 +1,67 @@
 package org.svenehrke.javafxdemos.address.util;
 
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 public class ZipRoutines {
+
+	public static <A,B,C> Iterable<C> zipWith(Iterable<A> iterableA, Iterable<B> iterableB, BiFunction<A,B,C> fn) {
+		return () -> new Iterator<C>() {
+			private Iterator<A> itA = iterableA.iterator();
+			private Iterator<B> itB = iterableB.iterator();
+
+			public boolean hasNext() {
+				return itA.hasNext() && itB.hasNext();
+			}
+
+			public C next() {
+				return fn.apply(itA.next(), itB.next());
+			}
+		};
+	}
+
+
+	public static<A, B, C> Stream<C> zip(Stream<? extends A> a,
+		Stream<? extends B> b,
+		BiFunction<? super A, ? super B, ? extends C> zipper) {
+		Objects.requireNonNull(zipper);
+		@SuppressWarnings("unchecked")
+		Spliterator<A> aSpliterator = (Spliterator<A>) Objects.requireNonNull(a).spliterator();
+		@SuppressWarnings("unchecked")
+		Spliterator<B> bSpliterator = (Spliterator<B>) Objects.requireNonNull(b).spliterator();
+
+		// Zipping looses DISTINCT and SORTED characteristics
+		int both = aSpliterator.characteristics() & bSpliterator.characteristics() &
+			~(Spliterator.DISTINCT | Spliterator.SORTED);
+		int characteristics = both;
+
+		long zipSize = ((characteristics & Spliterator.SIZED) != 0)
+			? Math.min(aSpliterator.getExactSizeIfKnown(), bSpliterator.getExactSizeIfKnown())
+			: -1;
+
+		Iterator<A> aIterator = Spliterators.iterator(aSpliterator);
+		Iterator<B> bIterator = Spliterators.iterator(bSpliterator);
+		Iterator<C> cIterator = new Iterator<C>() {
+			@Override
+			public boolean hasNext() {
+				return aIterator.hasNext() && bIterator.hasNext();
+			}
+
+			@Override
+			public C next() {
+				return zipper.apply(aIterator.next(), bIterator.next());
+			}
+		};
+
+		Spliterator<C> split = Spliterators.spliterator(cIterator, zipSize, characteristics);
+		return (a.isParallel() || b.isParallel())
+			? StreamSupport.stream(split, true)
+			: StreamSupport.stream(split, false);
+	}
+
 }
--- javafx8_tutorial/src/test/java/org/svenehrke/javafxdemos/address/util/ZipRoutinesTest.java
+++ javafx8_tutorial/src/test/java/org/svenehrke/javafxdemos/address/util/ZipRoutinesTest.java
@@ -1,14 +1,35 @@
 package org.svenehrke.javafxdemos.address.util;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class ZipRoutinesTest extends TestCase {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+import static java.util.stream.Collectors.joining;
+import static org.junit.Assert.assertEquals;
+import static org.svenehrke.javafxdemos.address.util.ZipRoutines.*;
+
+public class ZipRoutinesTest {
+
+	@Test
 	public void testZipWith() throws Exception {
+		Stream<String> strings = Arrays.asList("a", "b", "c").stream();
+		Stream<Integer> ints = Arrays.asList(6,9,14,32).stream();
+
+		String result = zip(strings, ints, (s, i) -> s + String.valueOf(i)).collect(joining("-"));
+
+		assertEquals("a6-b9-c14", result);
 
 	}
 
+	@Test
 	public void testZip() throws Exception {
-
+		List<String> strings = Arrays.asList("a","b","c");
+		List<Integer> ints = Arrays.asList(6, 9, 14, 32);
+		Iterable<String> result = zipWith(strings, ints, (s, i) -> s + i);
+		String stringResult = StreamSupport.stream(result.spliterator(), false).collect(joining("-"));
+		assertEquals("a6-b9-c14", stringResult);
 	}
 }
