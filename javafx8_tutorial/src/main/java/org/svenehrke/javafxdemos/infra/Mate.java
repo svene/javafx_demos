7c6c8722c4135fcdec4366f48211fecca6baf2a1
Sven Ehrke <sven.ehrke@canoo.com>
2014-12-24 13:09:17 +0100

    cleanup

--- javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/infra/Mate.java
+++ javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/infra/Mate.java
@@ -3,32 +3,11 @@ package org.svenehrke.javafxdemos.infra;
 import javafx.stage.Stage;
 import org.svenehrke.javafxdemos.address.Model;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
 public class Mate {
-
-	private Map<String, Runnable> commands = new HashMap<>();
 
 	private Model model;
 	Stage primaryStage;
 
-
-	public void addCommand(String commandId, Runnable command) {
-		commands.put(commandId, command);
-	}
-
-	public Optional<Runnable> getCommand(String commandId) {
-		return Optional.ofNullable(commands.get(commandId));
-	}
-
-	public void handleCommand(String commandId) {
-		Optional<Runnable> optional = getCommand(commandId);
-		if (optional.isPresent()) {
-			optional.get().run();
-		}
-	}
 
 	public Model getModel() {
 		return model;
