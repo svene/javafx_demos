ed8e1e7b77cf8276e25e6c7a1eb71e6cfeb3c165
Sven Ehrke <sven.ehrke@canoo.com>
2014-12-31 09:53:03 +0100

    removed superfluous dependency

--- javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/infra/FXMLLoader2.java
+++ javafx8_tutorial/src/main/java/org/svenehrke/javafxdemos/infra/FXMLLoader2.java
@@ -1,7 +1,6 @@
 package org.svenehrke.javafxdemos.infra;
 
 import javafx.fxml.FXMLLoader;
-import org.svenehrke.javafxdemos.address.Main;
 
 import java.io.IOException;
 import java.net.URL;
@@ -9,7 +8,7 @@ import java.net.URL;
 public class FXMLLoader2 {
 
 	public static <V, R> ViewAndRoot<V, R> loadFXML(String fxmlPath) {
-		URL resource = Main.class.getResource(fxmlPath);
+		URL resource = FXMLLoader2.class.getResource(fxmlPath);
 		final FXMLLoader loader = new FXMLLoader(resource, null);
 		try {
 			loader.load();
