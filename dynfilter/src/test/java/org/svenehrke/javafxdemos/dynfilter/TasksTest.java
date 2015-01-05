4724c1b7e7bc71663237268e7fc5f315328aaf07
Sven Ehrke <sven.ehrke@canoo.com>
2015-01-05 20:21:52 +0100

    demonstrate predicate workaround

--- dynfilter/src/test/java/org/svenehrke/javafxdemos/dynfilter/TasksTest.java
+++ dynfilter/src/test/java/org/svenehrke/javafxdemos/dynfilter/TasksTest.java
@@ -4,7 +4,6 @@ import javafx.beans.Observable;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.util.Callback;
@@ -16,10 +15,16 @@ import static org.junit.Assert.assertSame;
 public class TasksTest {
 
 	@Test
-	public void test1() throws Exception {
+	public void withPredicateReset() throws Exception {
+		m(true);
+	}
+	@Test
+	public void withoutPredicateReset() throws Exception {
+		m(false);
+	}
 
+	private void m(boolean resetPredicate) {
 		ObservableList<Task> allTasks = FXCollections.observableArrayList(Task.priorityExtractor());
-//		ObservableList<Task> prio1Tasks;
 		FilteredList<Task> prio1Tasks;
 
 		Task taskA = new Task("1", Task.PRIO_2);
@@ -38,42 +43,14 @@ public class TasksTest {
 
 
 		taskB.priority.setValue(Task.PRIO_1);
-		prio1Tasks.setPredicate(task -> Task.PRIO_1.equals(task.priority.getValue())); // without this the test fails! -> bug in filteredList
+		if (resetPredicate) {
+			prio1Tasks.setPredicate(task -> Task.PRIO_1.equals(task.priority.getValue())); // without this the test fails! -> bug in filteredList
+		}
 		assertEquals(2, allTasks.size());
 		assertEquals(1, prio1Tasks.size());
 		assertSame(taskB, prio1Tasks.get(0));
 
 		taskB.priority.setValue(Task.PRIO_2); // ArrayIndexOutOfBoundsException is thrown (and caught internally) leaving the lists in a corrupt state
-	}
-	@Test
-	public void test2() throws Exception {
-
-		ObservableList<Task> allTasks = FXCollections.observableArrayList(Task.priorityExtractor());
-//		ObservableList<Task> prio1Tasks;
-		FilteredList<Task> prio1Tasks;
-
-		Task taskA = new Task("1", Task.PRIO_1);
-		Task taskB = new Task("2", Task.PRIO_2);
-
-		prio1Tasks = new FilteredList<>(allTasks, task -> Task.PRIO_1.equals(task.priority.getValue()));
-		allTasks.addAll(taskA, taskB);
-
-		assertEquals(2, allTasks.size());
-		assertEquals(1, prio1Tasks.size());
-		assertSame(taskA, prio1Tasks.get(0));
-
-		taskA.priority.setValue(Task.PRIO_2);
-		assertEquals(2, allTasks.size());
-		assertEquals(0, prio1Tasks.size());
-
-
-		taskA.priority.setValue(Task.PRIO_1);
-		prio1Tasks.setPredicate(task -> Task.PRIO_1.equals(task.priority.getValue())); // without this the test fails! -> bug in filteredList
-		assertEquals(2, allTasks.size());
-		assertEquals(1, prio1Tasks.size());
-		assertSame(taskA, prio1Tasks.get(0));
-
-		taskA.priority.setValue(Task.PRIO_2); // ArrayIndexOutOfBoundsException is thrown (and caught internally) leaving the lists in a corrupt state
 	}
 
 	static class Task {
