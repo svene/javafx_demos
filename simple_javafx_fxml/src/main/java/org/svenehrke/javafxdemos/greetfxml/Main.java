67be528467c6870769c84106a3dbd4a9766cecc5
Sven Ehrke <sven.ehrke@canoo.com>
2015-11-05 06:40:48 +0100

    redesign config

--- simple_javafx_fxml/src/main/java/org/svenehrke/javafxdemos/greetfxml/Main.java
+++ simple_javafx_fxml/src/main/java/org/svenehrke/javafxdemos/greetfxml/Main.java
@@ -8,14 +8,13 @@ import javafx.stage.Stage;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static org.svenehrke.javafxdemos.greetfxml.ApplicationConfig.*;
+
 public class Main extends Application {
 
 	private final Logger logger = Logger.getLogger(Main.class.getName());
 
-	private static ApplicationSupporter applicationSupporter;
-
 	public static void main(String[] args) {
-		Main.applicationSupporter = new ApplicationSupporter("/greet.fxml", "bundles.app");
 		Application.launch(Main.class, args);
 	}
 
@@ -30,12 +29,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			FXMLLoader loader = new FXMLLoader(applicationSupporter.fxmlUrl(), applicationSupporter.resources());
+			FXMLLoader loader = new FXMLLoader(JavaFxApplications.fxmlUrl(FXML_URL), JavaFxApplications.resources(RESOURCE_BUNDLE_NAME));
 			loader.load();
 			new GUIBinder(loader.getController(), new PresentationState()).bindAndInitialize();
 
 			Scene scene = new Scene(loader.getRoot());
-			scene.getStylesheets().add(getClass().getResource("/app.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource(CSS_URL).toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle(loader.getResources().getString("title"));
 			primaryStage.show();
